10c9a138e064d493c8d3fb7220a600753f3961e0
Алекс <zagorik2000@mail.ru>
2020-02-01 22:49:08 +0300

    created links for create, update and delete player

--- lab2/esport/urls.py
+++ lab2/esport/urls.py
@@ -9,10 +9,9 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^players/$', views.PlayerListView.as_view(), name='players'),
     url(r'^player/(?P<pk>\d+)$', views.PlayerDetailView.as_view(), name='player-detail'),
-    # url(r'^player/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-player-librarian'),
-    url(r'^player/create/$', views.PlayerCreate.as_view(), name='player_create'),
-    url(r'^player/(?P<pk>\d+)/update/$', views.PlayerUpdate.as_view(), name='player_update'),
-    url(r'^player/(?P<pk>\d+)/delete/$', views.PlayerDelete.as_view(), name='player_delete'),
+    url(r'^player/create/$', views.PlayerCreate.as_view(), name='player-create'),
+    url(r'^player/(?P<pk>\d+)/update/$', views.PlayerUpdate.as_view(), name='player-update'),
+    url(r'^player/(?P<pk>\d+)/delete/$', views.PlayerDelete.as_view(), name='player-delete'),
 ]
 
 
